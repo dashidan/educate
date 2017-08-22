dc898ddf245b525205a301c9b75432c53b5adf94
dashidan <2903872229@qq.com>
2017-08-22 21:28:40 +0800

    增加HelloWorld

---
+++ java/base/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by dashidan.com on 2017/8/22.
+ * 输出HelloWorld.迈出程序第一步.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld.");
+    }
+}
